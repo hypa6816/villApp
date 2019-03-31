7799881a82533a0435bdf4ae539e08d11a78a7b5
Jonathan Fermin <hons.fermin@gmail.com>
2019-03-30 23:59:01 -0600

    koos did something brazy crazy

--- screens/ForumFeed.js
+++ screens/ForumFeed.js
@@ -1,40 +1,75 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
 import SafeAreaView from 'react-native-safe-area-view';
 import { StreamApp, FlatFeed } from 'react-native-activity-feed';
-import {STREAM_API_KEY, STREAM_API_TOKEN, STREAM_APP_ID} from 'react-native-dotenv';
+import {STREAM_API_KEY, STREAM_API_URL, STREAM_APP_ID} from 'react-native-dotenv';
+
 export default class ForumFeed extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      userToken: null,
+   }
+  }
   static navigationOptions = {
     header: null,
   };
 
+
+  async getUserToken(userID) {
+    let url = STREAM_API_URL + "/getUserStreamToken?userId=" + userID; 
+    try {
+      const response = await fetch(url)
+      return (await response.json()).userToken
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const userToken = await this.getUserToken();
+      this.setState({ userToken });
+    } catch (err) {
+      // handle errors
+    }
+  }
+
   render() {
     const api = STREAM_API_KEY;
-    const token = STREAM_API_TOKEN;
     const id =  STREAM_APP_ID;
     
+    if (this.state.userToken) {
+      // render child component that depends on async data
+      const token = this.state.userToken.toString()
+
+        return (
+          // <ScrollView style={styles.container}>
+          //   <Text>Welcome to FORUM FEED!</Text>
+          // </ScrollView>
     
-    return (
-      // <ScrollView style={styles.container}>
-      //   <Text>Welcome to FORUM FEED!</Text>
-      // </ScrollView>
-
-
-//       STREAM_API_KEY=kzjsxxee75ds
-// STREAM_API_TOKEN=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyZXNvdXJjZSI6ImFuYWx5dGljcyIsImFjdGlvbiI6IioiLCJ1c2VyX2lkIjoiKiJ9.qBhAUFdCUGg3JyAXd_u6zQRlvbgx4JHaKIt1NTE8XrA
-// STREAM_APP_ID=49446
-      <SafeAreaView style={{flex: 1}} forceInset={{ top: 'always' }}>
-      <StreamApp
-          apiKey="kzjsxxee75ds"
-          token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyZXNvdXJjZSI6ImFuYWx5dGljcyIsImFjdGlvbiI6IioiLCJ1c2VyX2lkIjoiKiJ9.qBhAUFdCUGg3JyAXd_u6zQRlvbgx4JHaKIt1NTE8XrA"
-          appId="49446"
-      >
-        <FlatFeed />
-      </StreamApp>
-    </SafeAreaView>
-    );
-  }
+          <SafeAreaView style={{flex: 1}} forceInset={{ top: 'always' }}>
+          <StreamApp
+              apiKey={api}
+              appId={id}
+              token={token}
+          >
+            <FlatFeed />
+          </StreamApp>
+        </SafeAreaView>
+        );
+      
+    } else {
+      return(
+        <View>
+          <Text>Loading...</Text>
+        </View>
+      );
+      // initial render
+    } 
+  } 
 }
 
 const styles = StyleSheet.create({
